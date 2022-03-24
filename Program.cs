7d5fbe92468f9e0b8a7e6f8020f7be96a9e60ff3
atshibko <atshibko@gmail.com>
2022-03-24 10:28:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LineToPolyclinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInLine;
+            int timeForOnePersonWaitingInMinutes = 10;
+            int timeToWaitMinutes;
+            int timeToWaitHours;
+
+            Console.WriteLine("Введите каолличество людей в очереди:");
+            peopleInLine = int.Parse(Console.ReadLine());
+            timeToWaitHours = peopleInLine * timeForOnePersonWaitingInMinutes / 60;
+            timeToWaitMinutes = peopleInLine * timeForOnePersonWaitingInMinutes % 60;
+            Console.WriteLine($"Ваше время ожидания в очереди {timeToWaitHours} часов и {timeToWaitMinutes} минут.");
+            Console.ReadKey();
+        }
+    }
+}
